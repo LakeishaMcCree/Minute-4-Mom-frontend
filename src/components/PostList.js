0c2057490158c02c523a2271a3daa1d7bab8de00
github username <lmccree215@icloud.comgit config --global user.name LakeishaMcCree>
2021-01-24 19:04:51 -0500

    set up links to route to the post lists

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,14 +1,17 @@
 import React from 'react' 
+import {Route, Link} from 'react-router-dom'
 import PostShow from './PostShow' 
 
 //functional component because it will just going to present a list of posts
 const PostList = (props) => {
 
     return (
-        <div>
+        <li>
             {props.posts.map(post => 
-            <div key={post.id}><PostShow post={post}/></div>)}
-        </div>
+            <div key={post.id}>
+                <Link path={`/posts/${post.id}`}>{post.name}</Link>
+            </div> )}
+        </li>
 
     )
 
--- src/components/PostShow.js
+++ src/components/PostShow.js
@@ -3,16 +3,16 @@ import React from 'react'
 
 const PostShow = (props) => {
 
-console.log(props) 
+    console.log(props) 
 
-let post = props.posts[props.match.params.id - 1]
-console.log(post)
+    let post = props.posts[props.match.params.id - 1]
+    // console.log(post)
 
-return (
-    <li>
-      {post ? post.title :null} - {post ? post.author :null}
-    </li>
-)
+    return (
+        <li>
+        {post ? post.title :null} - {post ? post.author :null}
+        </li>
+    )
 
 
 
